9a005ecff3509076fd4b4654e35ccd2f63f029f8
VelupeFerin <2235062013@st.usst.edu.cn>
2026-06-09 15:36:16 +0800

    优化获取cookies的独立程序的操作提示文案

--- get_ydmap-usst_cookies_file.py
+++ get_ydmap-usst_cookies_file.py
@@ -3,17 +3,20 @@ import nodriver as nd
 
 async def main():
     input(
-        "即将打开Chrome浏览器（由本程序打开，若未打开则说明没有安装Chrome或打开过程中出现问题），并跳转到USST智慧体育个人中心。请登录然后回到此处。点击Enter继续")
+        "即将打开Chrome浏览器（由本程序打开，若未打开则说明没有安装Chrome或打开过程中出现问题），并跳转到USST智慧体育个人中心。请登录然后回到此处。点击Enter打开浏览器")
     browser = await nd.start()
     await browser.get('https://usst.ydmap.cn/user/my')
     cookies_file_name = input("请命名您的cookies：")
     await browser.cookies.save(cookies_file_name + '.cookies')
     browser.stop()
-    input("您的cookies已保存到本程序所在文件夹\n接下来要检查cookies的正确性,浏览器打开后，回到此处。点击Enter继续")
+    input("您的cookies已保存到本程序所在文件夹\n接下来要检查cookies是否有效。点击Enter继续")
     browser = await nd.start()
     await browser.get('https://usst.ydmap.cn/user/my')
-    await browser.cookies.load(cookies_file_name + '.cookies')
-    input("如果在“个人中心”发现您已登录，那么刚才导出的cookies是正确的。点击Enter关闭")
+    try:
+        await browser.cookies.load(cookies_file_name + '.cookies')
+        input("如果在USST智慧体育个人中心发现您已登录，那么刚才导出的cookies是有效的。点击Enter关闭")
+    except Exception:
+        input("打开cookies时出现异常，请重新执行本程序")
     browser.stop()
 
 
